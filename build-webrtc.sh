91739518457b128aef223ac8f2b9ffcea124eb4d
Roman Skvirsky <rskvirsky@thevideocafe.com>
2014-08-04 11:27:43 +0400

    updated build files

--- build-webrtc.sh
+++ build-webrtc.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# gclient can be found here:
-# git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-
-# don't forget to add gclient to path environment variable:
-# export PATH=$PATH:/path/to/depot_tools
-
 fail() {
     echo "*** webrtc build failed"
     exit 1
@@ -29,57 +23,36 @@ set_environment_for_x86() {
    export GYP_GENERATOR_FLAGS="$GYP_GENERATOR_FLAGS output_dir=out_x86"
 }
 
-build_arm() {
-    echo "-- building webrtc/arm"
+build() {
+    echo "-- building webrtc/$1"
     pushd trunk || fail
-    set_environment_for_arm || fail
+    set_environment_for_$1 || fail
 	gclient sync --force || fail
     gclient runhooks --force || fail
-    ninja -C out_arm/Debug libjingle_peerconnection_so || fail
-	ninja -C out_arm/Release libjingle_peerconnection_so || fail
-    pushd out_arm/Release || fail
-    #strip -S -x -o libWebRTC-armv7-stripped.a -r libWebRTC-armv7.a
+    ninja -C out_$1/Debug libjingle_peerconnection_so libjingle_peerconnection.jar || fail
+	ninja -C out_$1/Release libjingle_peerconnection_so libjingle_peerconnection.jar || fail
+    pushd out_$1/Release || fail
     popd
     popd
-    echo "-- webrtc/arm has been sucessfully built"
+    echo "-- webrtc/$1 has been sucessfully built"
 }
 
-build_x86() {
-    echo "-- building webrtc/x86"
-    pushd trunk || fail
-    set_environment_for_x86 || fail
-	gclient sync --force || fail
-    gclient runhooks --force || fail
-    ninja -C out_x86/Debug libjingle_peerconnection_so || fail
-	ninja -C out_x86/Release libjingle_peerconnection_so || fail
-    pushd out_x86/Release || fail
-    #mv libWebRTC-ia32-stripped.a libWebRTC-ia32.a
+prerequisites() {
+    export PATH=`pwd`/depot_tools:"$PATH"
+    which gclient >/dev/null
+    if [ $? -ne 0 ]; 
+    then
+	   git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+	   export PATH=`pwd`/depot_tools:"$PATH"
+    fi
+    gclient sync --nohooks
+    pushd trunk
+    source ./build/android/envsetup.sh
     popd
-    popd
-    echo "-- webrtc/x86 has been sucessfully built"
 }
 
-create_framework() {
-    echo "-- creating webrtc framework"
-    echo "-- webrtc framework created"
-}
+prerequisites
 
-installdepottools() {
-which gclient >/dev/null
-if [ $? -ne 0 ]; 
-then
-	git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-	export PATH=`pwd`/depot_tools:"$PATH"
-fi
-}
-
-installdepottools
-
-
-gclient sync --nohooks
-pushd trunk
-source ./build/android/envsetup.sh
-popd
-build_arm
-build_x86
+build arm
+build x86
 
